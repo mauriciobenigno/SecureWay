27b71831ae7270974d344b6cf964501d6947c0c0
Mauricio Benigno <mauriciobenigno@hotmail.com>
2020-12-04 20:30:43 -0300

    Commit Inicial

---
+++ app/src/main/java/com/mauriciobenigno/secureway/MapsActivity.kt
@@ -0,0 +1,91 @@
+package com.mauriciobenigno.secureway
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.TileOverlayOptions
+import com.google.maps.android.heatmaps.HeatmapTileProvider
+import com.google.maps.android.heatmaps.WeightedLatLng
+import org.json.JSONArray
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_maps)
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    override fun onMapReady(googleMap: GoogleMap) {
+        val data = generateHeatMapData()
+
+        val heatMapProvider = HeatmapTileProvider.Builder()
+            .weightedData(data) // load our weighted data
+            .radius(50) // optional, in pixels, can be anything between 20 and 50
+            .maxIntensity(1000.0) // set the maximum intensity
+            .build()
+
+        googleMap?.addTileOverlay(TileOverlayOptions().tileProvider(heatMapProvider))
+
+        val indiaLatLng = LatLng(20.5937, 78.9629)
+        googleMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(indiaLatLng, 5f))
+        /*
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
+    }
+
+    private fun generateHeatMapData(): ArrayList<WeightedLatLng> {
+        val data = ArrayList<WeightedLatLng>()
+
+        val jsonData = getJsonDataFromAsset("district_data.json")
+        jsonData?.let {
+            for (i in 0 until it.length()) {
+                val entry = it.getJSONObject(i)
+                val lat = entry.getDouble("lat")
+                val lon = entry.getDouble("lon")
+                val density = entry.getDouble("density")
+
+                if (density != 0.0) {
+                    val weightedLatLng = WeightedLatLng(LatLng(lat, lon), density)
+                    data.add(weightedLatLng)
+                }
+            }
+        }
+
+        return data
+    }
+
+    private fun getJsonDataFromAsset(fileName: String): JSONArray? {
+        try {
+            val jsonString = assets.open(fileName).bufferedReader().use { it.readText() }
+            return JSONArray(jsonString)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            return null
+        }
+    }
+
+}
